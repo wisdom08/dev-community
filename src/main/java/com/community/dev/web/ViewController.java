0f59fe8b0f3432683b8588eb58a55640d33da0d0
wisdom08 <wisdom08@kakao.com>
2022-09-25 08:26:52 +0900

    fix: 테스트용 homeController 제거하고 ViewController 한 곳에서 메인페이지와 로그인 요청에 응답함
    - 추가적으로 패키지 변경함

--- src/main/java/com/community/dev/web/ViewController.java
+++ src/main/java/com/community/dev/web/ViewController.java
@@ -1,5 +1,7 @@
-package com.community.dev.view;
+package com.community.dev.web;
 
+import com.community.dev.config.auth.SessionUser;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +10,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ViewController {
 
+    private final HttpSession httpSession;
+
+    public ViewController(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+
     @GetMapping("/social")
     public String socialSuccess(Model model,
         @RequestParam(value = "provider", required = false) String provider,
         @RequestParam(value = "oauthId", required = false) String oauthId
     ) {
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         model.addAttribute("provider", provider);
         model.addAttribute("oauthId", oauthId);
-        return "social-success";
+        return "index";
     }
 }
